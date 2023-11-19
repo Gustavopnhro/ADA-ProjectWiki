4c20702212598d0a8f12d9b6c9eb7f50930764af
gustavo <gustavo.pnhro@gmail.com>
2023-11-19 14:37:02 -0300

    feat: inserting main script

--- main_script.py
+++ main_script.py
@@ -1,11 +1,12 @@
 import os
 import markdown
 from jinja2 import Environment, FileSystemLoader
+import json
 
-def generate_html_post(title, content):
+def generate_html_post(general, content):
     env = Environment(loader=FileSystemLoader("templates"))
     template = env.get_template("post_template.html")
-    return template.render(title=title, content=content)
+    return template.render(title=general['title'], author=general['author'], description=general['description'], content=content)
 
 def index_page(posts):
     env = Environment(loader=FileSystemLoader("templates"))
@@ -14,26 +15,37 @@ def index_page(posts):
 
 markdown_directory = "markdown_files"
 output_directory = "pages"
+json_path = "json_path"
 
 markdown_files_list = [f for f in os.listdir(markdown_directory) if f.endswith(".md")]
 
 posts = []
 
-for markdown_file in markdown_files_list:
+for index, markdown_file in enumerate(markdown_files_list):
     with open(os.path.join(markdown_directory, markdown_file), "r", encoding="utf-8") as post:
         markdown_content = post.read()
+        content_lines = markdown_content.split("\n")
 
+    content_index = 0
+    info_parts = []
+    for i in range (0,3):
+        split_text = content_lines[i].split(':')
+        content_index += len(content_lines[i])+1
+        info_parts.append(split_text[1].strip())
+        
+    general_info = {"title": str(info_parts[0]), "description": str((info_parts[1])), "author": str(info_parts[2])}
 
-    html_content = markdown.markdown(markdown_content, extensions=["markdown.extensions.fenced_code"])
-    
-    post_title = (os.path.splitext(markdown_file)[0])
+    with open(os.path.join(json_path, f"general_info[{index+1}].json") , "w+", encoding="utf-8") as json_file:
+        json.dump(general_info, json_file)
 
-    html_post = generate_html_post(post_title, html_content)
-
+    html_content = markdown.markdown(markdown_content[content_index::], extensions=["markdown.extensions.fenced_code"])
+    post_title = general_info["title"]
+    html_post = generate_html_post(general_info, html_content)
     with open(os.path.join(output_directory, f"{post_title}.html"), "w", encoding="utf-8") as output_file:
         output_file.write(html_post)
+        posts.append({"title": general_info['title'], "link":f"{general_info['title']}.html", "preview": general_info['description']+"..."})
 
-    posts.append({"title": post_title, "link":f"{post_title}.html", "preview":markdown_content[0:20]+"..."})
+
 
 
 index_html = index_page(posts)
